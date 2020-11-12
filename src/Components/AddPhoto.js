053c79599acd050f87bdbab3617bfd4c6ee065d8
Bartlomiej Janik <bartlomiejjanik1@gmail.com>
2020-11-12 17:59:21 +0100

    Pobranie value z formularza wprowadzonego przez usera i print na ocnsoli - proba1

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -8,7 +8,8 @@ class AddPhoto extends Component {
     }
     handleSubmit(event) {
         event.preventDefault();
-    
+        console.log(event.target.elements.link.value)
+        console.log(event.target.elements.description.value)
     }
 
     render() {
@@ -17,9 +18,9 @@ class AddPhoto extends Component {
         <h1> Biblioteka ksiazek </h1>
         <div className="form">
         <form onSubmit={this.handleSubmit}>  
-            <input type = "text" placeholder="Link do zdjecia" />
-            <input type = "text" placeholder="Opis" />
-            <input type = "text" placeholder="Cena" />
+            <input type = "text" placeholder="Link do zdjecia" name="link"/>
+            <input type = "text" placeholder="Opis" name="description" />
+            <input type = "text" placeholder="Cena" name="price" />
             <button> Dodaj </button>
         </form>
         </div>
