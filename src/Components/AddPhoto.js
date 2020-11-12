985df1ae478a1984ac89468c2a3b4304ddcda732
Bartlomiej Janik <bartlomiejjanik1@gmail.com>
2020-11-12 14:39:24 +0100

    Dodalem formularz do wprowadzenia linku do zdjecia ksiazki

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -2,7 +2,19 @@ import React, {Component} from 'react'
 
 class AddPhoto extends Component {
     render() {
-        return <h1> This is the page where we will add photos </h1>
+        return (
+    <div>
+        <h1> Biblioteka ksiazek </h1>
+        <div className="form">
+        <form>  
+            <input type = "text" placeholder="Link do zdjecia" />
+            <input type = "text" placeholder="Opis" />
+            <input type = "text" placeholder="Cena" />
+        </form>
+        </div>
+    </div> 
+
+    )
     }
 }
 
