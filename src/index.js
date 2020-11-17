8aacfcfc65126331160bf7f12273c8221a55293c
Bartlomiej Janik <bartlomiejjanik1@gmail.com>
2020-11-17 10:07:47 +0100

    Dodalem bootstrap - poczatek

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Main from './Components/Main'
 import './styles/stylesheet.css'
 import {BrowserRouter} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 
